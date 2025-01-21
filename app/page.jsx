d8eb001decaeb1f5adcc1070dc41a1f7d01da3ba
Delgorithm <delgorithm@gmail.com>
2025-01-21 19:33:48 +0100

    playing with r3f

--- app/page.jsx
+++ app/page.jsx
@@ -5,18 +5,18 @@ import { Canvas } from '@react-three/fiber'
 
 export default function Page() {
   return (
-    <>
+    <div className='bg-black h-full w-full'>
       <Canvas
         shadows
         camera={{
           fov: 45,
           near: 0.1,
           far: 120,
-          position: [-4, 3, 6],
+          position: [0, 1, 5],
         }}
       >
         <HiroContainerDisplay />
       </Canvas>
-    </>
+    </div>
   )
 }
--- src/components/display/hiro-container-display.tsx
+++ src/components/display/hiro-container-display.tsx
@@ -1,17 +1,23 @@
 import { HiroContainerModel } from '../model/hiro-container-model.tsx'
-import { OrbitControls } from '@react-three/drei'
+import { Center, Float, OrbitControls, ScrollControls } from '@react-three/drei'
 import { Suspense } from 'react'
 
 export default function HiroContainerDisplay() {
   return (
     <>
-      <OrbitControls makeDefault />
+      <OrbitControls enableRotate={false} enableZoom={false} />
 
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <Suspense fallback={<p>Chargement...</p>}>
-        <HiroContainerModel />
+      <Suspense fallback={null}>
+        <Float>
+          <ScrollControls pages={2} damping={0.75}>
+            <Center>
+              <HiroContainerModel rotation-y={[-0.55]} />
+            </Center>
+          </ScrollControls>
+        </Float>
       </Suspense>
     </>
   )
